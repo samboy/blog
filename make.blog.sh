d5c34acf2ec3d91356bc767bd0b37b9539ac020f
Sam Trenholme <strenholme.usenet@gmail.com>
2025-08-25 10:27:48 -0600

    Newest first for blog

--- make.blog.sh
+++ make.blog.sh
@@ -4,7 +4,7 @@
 
 echo \<h1\>Sam Trenholme’s blog\</h1\> > index.html
 echo >> index.html
-for a in embed/*embed ; do
+for a in $( ls embed/*embed | sort -r ) ; do
   cat $a >> index.html
   echo $a
 done
