51ca64bf8d8b9760d93f6619f4e0356f5e7786fc
Sam Trenholme <strenholme.usenet@gmail.com>
2025-09-06 14:54:34 -0600

    Begin work on converting Markdown to my blog’s “embed” format
    Markdown is a standard format and the one I plan to use for the
    source of my blog entries from now on.

---
+++ markdown/exmaple.md
@@ -0,0 +1,28 @@
+![blogpic](pics/2024-05-01.jpg)
+# Blog Title
+## February 31, 2029
+
+This is some text for an example blog entry.
+
+### How are you
+
+I am [doing well](https://samiam.org).
+
+áéíóúñ—‘’“”
+
+Lorem ipsum dolor sit amet, “consectetur adipiscing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+commodo consequat”. Duis aute irure dolor’s in reprehenderit in voluptate
+velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+id est laborum.
+
+### A picture
+![widepic](pics/marshall_red_vwbug.jpg)
+_A red VW Bug means good luck_
+
+### Another section
+
+Some more text.
+
---
+++ markdown/markdown2embed.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+cat > /dev/null <<EOF
+This converts Markdown in to the .embed HTML format my blog uses.
+
+This massages the output of htmldoc to make it suitable for my web
+pages.
+
+The argument is the filename to convert, e.g. foo.md
+
+Output is on standard output.
+
+This script assumes htmldoc 1.9.14.
+
+The markdown needs to have as its headers lines like this:
+
+EOF
+
+if [ -z "$1" ] ; then
+    echo Usage: $0 filename.md
+    echo Output is on standard output
+    exit 1
+fi
+
+FILENAME="$1"
+
+htmldoc --charset utf-8 $FILENAME 2>/dev/null | awk '
+		{if(a==2){print}if(/HR NOSHADE/){a++}}
+	' | awk '{if(/img alt="blogpic"/) { # Fix top image
+		pic = $3
+ 		sub(/^[^"]*"/,"",pic)
+ 		sub(/"[^"]*$/,"",pic)
+		print "<div class=blogpicx style=\"text-align: right\">"
+		print "<img src=\"" pic "\" width=224 height=126>"
+		print "</div>" } else {print}}' | awk '{ # Other images
+		  if(/img alt="widepic"/) {
+		pic = $3
+		caption = $4 " " $5 " " $6
+ 		sub(/^[^"]*"/,"",pic)
+ 		sub(/"[^"]*$/,"",pic)
+		print "<img class=widepic src=\"" pic "\" alt=\"\">"
+		print "<br class=nomobile>"
+		inwidepic = 1
+		} else if(inwidepic) {
+			print caption
+			print
+			inwidepic = 0
+		} else {print}}' | awk ' # Footer
+		{print} END {
+		print ""
+		print "<p>"
+		print ""
+		print "<A href=/blog/>Blog index</A>"
+		print ""
+		print "</div>"
+		print ""
+		}'
+
+
