aedadb9401607ed67cfd8d0349d07f0738295dd6
Sam Trenholme <strenholme.usenet@gmail.com>
2025-09-07 08:41:56 -0600

    The blog filter now uses Lua(ncy) instead of an awk/perl hack

--- markdown/markdown2embed.sh
+++ markdown/markdown2embed.sh
@@ -12,6 +12,8 @@ Output is on standard output.
 
 This script assumes htmldoc 1.9.14.
 
+lunacy64 is a compile of lunacy (Lua 5.1 variant)
+
 The markdown needs to have as its headers lines like this:
 
 EOF
@@ -22,37 +24,13 @@ if [ -z "$1" ] ; then
     exit 1
 fi
 
-FILENAME="$1"
+FILENAME="$(/bin/pwd)/$1"
+echo $FILENAME
+SCRIPTPATH="$0"
+SCRIPTPATH=${SCRIPTPATH%/*}
+if [ ! -z "$SCRIPTPATH" ] ; then
+	cd $SCRIPTPATH
+fi
 
-htmldoc --charset utf-8 $FILENAME 2>/dev/null | awk '
-		{if(a==2){print}if(/HR NOSHADE/){a++}}
-	' | awk '{if(/img alt="blogpic"/) { # Fix top image
-		pic = $3
- 		sub(/^[^"]*"/,"",pic)
- 		sub(/"[^"]*$/,"",pic)
-		print "<div class=blogpicx style=\"text-align: right\">"
-		print "<img src=\"" pic "\" width=224 height=126>"
-		print "</div>" } else {print}}' | awk '{ # Other images
-		  if(/img alt="widepic"/) {
-		pic = $3
-		caption = $4 " " $5 " " $6 " " $7 " " $8 " " $9 " " $10 " " $11
- 		sub(/^[^"]*"/,"",pic)
- 		sub(/"[^"]*$/,"",pic)
-		print "<img class=widepic src=\"" pic "\" alt=\"\">"
-		print "<br class=nomobile>"
-		inwidepic = 1
-		} else if(inwidepic) {
-			print caption
-			print
-			inwidepic = 0
-		} else {print}}' | awk ' # Footer
-		{print} END {
-		print ""
-		print "<p>"
-		print ""
-		print "<A href=/blog/>Blog index</A>"
-		print ""
-		print "</div>"
-		print ""
-		}' | perl -pe ' # This perl mess removes <a id> tags
-        s/<a id[^>]+>([^<]+)<\/a>/\1/;s/<a id[^>]+>//;s|</a>(</h\d>)|\1|'
+htmldoc --charset utf-8 $FILENAME 2>/dev/null | ./lunacyBlogFilter
+
