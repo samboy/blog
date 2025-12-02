a637a4318899656b042568c37bffc6bea391887e
Sam Trenholme <strenholme.usenet@gmail.com>
2025-12-01 20:31:52 -0600

    Give links color so we don’t see visited links.
    Also, give the blog a little bit more of the style of my main
    webpage.

--- make.blog.sh
+++ make.blog.sh
@@ -21,7 +21,9 @@ echo '@media screen and (max-width: 640px) { ' >> index.html
 echo '.nomobile { display: none; } }' >> index.html
 
 # A lot of the blog was made in an era before phone screens were mainstream
-echo 'body { max-width: 95vw; overflow-x: hidden; }' >> index.html
+echo 'body { max-width: 95vw; overflow-x: hidden; } ' >> index.html
+
+echo 'a { color: #258723; } ' >> index.html
 
 # Nav after blog entries
 echo '.GitBlogNav { display: table; width: 95vw; ' >> index.html
